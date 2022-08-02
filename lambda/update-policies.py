624000be425011e96b4e819051fb38b9f56c8187
Sourcery AI <>
2022-08-02 19:26:17 +0000

    'Refactored by Sourcery'

--- lambda/update-policies.py
+++ lambda/update-policies.py
@@ -25,96 +25,97 @@ def lambda_handler(event, context):
         json_message = json.loads(message)
         Lifecycle_Transition = json_message['Event']
         if Lifecycle_Transition == "autoscaling:EC2_INSTANCE_LAUNCH":
-            instance_id = json_message['EC2InstanceId']
-            print(instance_id)
-            update_iam_policy = update_iam_inline_policy(instance_id)
-            update_secret_manager = update_secret_manager_policy(instance_id)
-            update_bucket_policy = update_s3_bucket_policy(BUCKET_NAME,instance_id)
-            update_iam_managed_policy = update_managed_policy(MANAGED_POLICY_ARN,instance_id)
-            if update_iam_managed_policy:
-                print("SUCCESS")
-            else:
-                print("something failed: FAILURE")
+                instance_id = json_message['EC2InstanceId']
+                print(instance_id)
+                update_iam_policy = update_iam_inline_policy(instance_id)
+                update_secret_manager = update_secret_manager_policy(instance_id)
+                update_bucket_policy = update_s3_bucket_policy(BUCKET_NAME,instance_id)
+                if update_iam_managed_policy := update_managed_policy(
+                    MANAGED_POLICY_ARN, instance_id):
+                        print("SUCCESS")
+                else:
+                        print("something failed: FAILURE")
        
         
 def update_iam_inline_policy(instance_id):
-    try:
-        response = iam.get_role_policy(RoleName=ROLE_NAME,
-                PolicyName=POLICY_NAME)
-        policy_document = response['PolicyDocument']
-        inst_user_id = policy_document['Statement'][0]['Condition'
-                ]['StringNotLike']['aws:userid'][0]
-        split_string = inst_user_id.split(':')
-        instance_id_old = split_string[1]
-        # convert dictionary object into string to replace the instance id
-        result = json.dumps(policy_document)
-        temp_policy_doc = result.replace(instance_id_old, instance_id)
-        #print(temp_policy_doc)
-        updated_policy = json.dumps(temp_policy_doc)
-        response = iam.put_role_policy(RoleName=ROLE_NAME,
-                PolicyName=POLICY_NAME,
-                PolicyDocument=temp_policy_doc)
-        print(response)
-    except botocore.exceptions.ClientError as e:
-        log("Error updating the inline policy {}: {}".format(instance_id, e.response['Error'])) 
+        try:
+                response = iam.get_role_policy(RoleName=ROLE_NAME,
+                        PolicyName=POLICY_NAME)
+                policy_document = response['PolicyDocument']
+                inst_user_id = policy_document['Statement'][0]['Condition'
+                        ]['StringNotLike']['aws:userid'][0]
+                split_string = inst_user_id.split(':')
+                instance_id_old = split_string[1]
+                # convert dictionary object into string to replace the instance id
+                result = json.dumps(policy_document)
+                temp_policy_doc = result.replace(instance_id_old, instance_id)
+                #print(temp_policy_doc)
+                updated_policy = json.dumps(temp_policy_doc)
+                response = iam.put_role_policy(RoleName=ROLE_NAME,
+                        PolicyName=POLICY_NAME,
+                        PolicyDocument=temp_policy_doc)
+                print(response)
+        except botocore.exceptions.ClientError as e:
+                log(f"Error updating the inline policy {instance_id}: {e.response['Error']}") 
 
 def update_secret_manager_policy(instance_id):
-  try:
-    response = secret_client.get_resource_policy(SecretId=AD_SECRET)
-    policy_document = response['ResourcePolicy']
-    #print(policy_document)
-    json_ver = json.loads(policy_document)
-    inst_user_id = json_ver['Statement'][0]['Condition']['StringNotLike']['aws:userid'][0]
-    split_string = inst_user_id.split(':')
-    instance_id_old = split_string[1]
-    result = json.dumps(json_ver)
-    temp_policy_doc = result.replace(instance_id_old, instance_id)
-    #print(temp_policy_doc)
-    updated_policy = temp_policy_doc
-    response = secret_client.put_resource_policy(SecretId=AD_SECRET,ResourcePolicy=temp_policy_doc)
-    print(response)
-  except  botocore.exceptions.ClientError as e:
-        log("Error updating the secret manager policy {}: {}".format(instance_id, e.response['Error']))	
+        try:
+                response = secret_client.get_resource_policy(SecretId=AD_SECRET)
+                policy_document = response['ResourcePolicy']
+                #print(policy_document)
+                json_ver = json.loads(policy_document)
+                inst_user_id = json_ver['Statement'][0]['Condition']['StringNotLike']['aws:userid'][0]
+                split_string = inst_user_id.split(':')
+                instance_id_old = split_string[1]
+                result = json.dumps(json_ver)
+                temp_policy_doc = result.replace(instance_id_old, instance_id)
+                #print(temp_policy_doc)
+                updated_policy = temp_policy_doc
+                response = secret_client.put_resource_policy(SecretId=AD_SECRET,ResourcePolicy=temp_policy_doc)
+                print(response)
+        except botocore.exceptions.ClientError as e:
+                log(f"Error updating the secret manager policy {instance_id}: {e.response['Error']}"
+                    )	
 	
 def update_s3_bucket_policy(bucket_name,instance_id):
-  try:
-    response = s3.get_bucket_policy(Bucket=bucket_name)
-    policy_document = response['Policy']
-    json_ver = json.loads(policy_document)
-    inst_user_id = json_ver['Statement'][0]['Condition']['StringNotLike']['aws:userid'][0]
-    split_string = inst_user_id.split(':')
-    instance_id_old = split_string[1]
-    # convert dictionary object into string to replace the instance id
-    result = json.dumps(json_ver) 
-    temp_policy_doc = result.replace(instance_id_old, instance_id)
-    updated_policy = temp_policy_doc
-    response = s3.put_bucket_policy(Bucket=bucket_name,Policy=updated_policy)
-    print(response)
-  except  botocore.exceptions.ClientError as e:
-        log("Error updating S3 bucket policy {}: {}".format(instance_id, e.response['Error']))	
+        try:
+                response = s3.get_bucket_policy(Bucket=bucket_name)
+                policy_document = response['Policy']
+                json_ver = json.loads(policy_document)
+                inst_user_id = json_ver['Statement'][0]['Condition']['StringNotLike']['aws:userid'][0]
+                split_string = inst_user_id.split(':')
+                instance_id_old = split_string[1]
+                # convert dictionary object into string to replace the instance id
+                result = json.dumps(json_ver) 
+                temp_policy_doc = result.replace(instance_id_old, instance_id)
+                updated_policy = temp_policy_doc
+                response = s3.put_bucket_policy(Bucket=bucket_name,Policy=updated_policy)
+                print(response)
+        except botocore.exceptions.ClientError as e:
+                log(f"Error updating S3 bucket policy {instance_id}: {e.response['Error']}")	
 
 def update_managed_policy(policy_arn,instance_id):
-  var4 = None
-  try:
-    response = iam.get_policy(PolicyArn=policy_arn)
-    policy_detail = response['Policy']
-    policy_version = policy_detail['DefaultVersionId']
-    response2 = iam.get_policy_version(PolicyArn=policy_arn,VersionId=policy_version)
-    policy_body = response2['PolicyVersion']['Document']
-    inst_user_id = policy_body['Statement'][0]['Resource'][1]
-    split_string = inst_user_id.split(':instance/')
-    instance_id_old = split_string[1]
-    result = json.dumps(policy_body) 
-    temp_policy_doc = result.replace(instance_id_old, instance_id)
-    #print(temp_policy_doc)
-    response = iam.create_policy_version(PolicyArn=policy_arn,PolicyDocument=temp_policy_doc,SetAsDefault= True)
-    response = iam.delete_policy_version(PolicyArn= policy_arn,VersionId=policy_version)
-    #print(response)	
-    var4 = response['ResponseMetadata']['HTTPStatusCode']	
-  except botocore.exceptions.ClientError as e:
-        log("Error updating IAM managed policy {}: {}".format(instance_id, e.response['Error']))
-  return var4	
+        var4 = None
+        try:
+                response = iam.get_policy(PolicyArn=policy_arn)
+                policy_detail = response['Policy']
+                policy_version = policy_detail['DefaultVersionId']
+                response2 = iam.get_policy_version(PolicyArn=policy_arn,VersionId=policy_version)
+                policy_body = response2['PolicyVersion']['Document']
+                inst_user_id = policy_body['Statement'][0]['Resource'][1]
+                split_string = inst_user_id.split(':instance/')
+                instance_id_old = split_string[1]
+                result = json.dumps(policy_body) 
+                temp_policy_doc = result.replace(instance_id_old, instance_id)
+                #print(temp_policy_doc)
+                response = iam.create_policy_version(PolicyArn=policy_arn,PolicyDocument=temp_policy_doc,SetAsDefault= True)
+                response = iam.delete_policy_version(PolicyArn= policy_arn,VersionId=policy_version)
+                #print(response)	
+                var4 = response['ResponseMetadata']['HTTPStatusCode']
+        except botocore.exceptions.ClientError as e:
+                log(f"Error updating IAM managed policy {instance_id}: {e.response['Error']}")
+        return var4	
 
 def log(error):
-    print('{}Z {}'.format(datetime.utcnow().isoformat(), error))
+        print(f'{datetime.utcnow().isoformat()}Z {error}')
 	
